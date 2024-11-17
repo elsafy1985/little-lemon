d7abee4c61660e0592d90efa1942a71ee0dfa904
Mousa Elsaify <elsafy1985@gmail.com>
2024-11-17 10:30:32 +0300

    html Changes

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Nav from "./Nav";
+
+export default function Header() {
+  return (
+    <header>
+      <img src="images/logo.png" alt="lemon" />
+      <Nav />
+    </header>
+  );
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/contact">Menu</a>
+        </li>
+        <li>
+          <a href="/blog">Reservations</a>
+        </li>
+        <li>
+          <a href="/blog">Order Online</a>
+        </li>
+        <li>
+          <a href="/blog">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
